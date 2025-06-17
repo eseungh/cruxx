c66170d6477ca776012a02b367863bccea53febb
eseungh <eseungh@gmail.com>
2025-06-17 20:06:32 +0900

    Initial Commit

---
+++ cruxx/cruxxApp.swift
@@ -0,0 +1,17 @@
+//
+//  cruxxApp.swift
+//  cruxx
+//
+//  Created by Lee Seung Ho on 6/17/25.
+//
+
+import SwiftUI
+
+@main
+struct cruxxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
